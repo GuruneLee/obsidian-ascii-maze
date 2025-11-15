d810b066e1b5f0c85ee917d84b477df19fe10ce5
gurunelee <dlckdgk4858@gmail.com>
2025-11-15 19:57:23 +0900

    feat: GameInformationField 및 TimeModule 정의
    - Field: View 에서 ROW 단위의 영역을 책임지는 컴포넌트
    - Module: Field 에서 COLUMN 단위의 영역을 책임지는 컴포넌트
    
    ┌─────────────────────────────────────────┐
    │ Field (ROW 단위)                         │
    │ ┌─────────┬─────────┬─────────┬───────┐ │
    │ │ Module  │ Module  │ Module  │ ...   │ │
    │ │ (COL 1) │ (COL 2) │ (COL 3) │       │ │
    │ └─────────┴─────────┴─────────┴───────┘ │
    └─────────────────────────────────────────┘

---
+++ src/GameInformationField.ts
@@ -0,0 +1,40 @@
+import {TimerModule} from "./TimerModule";
+
+export class GameInformationField {
+	stage: number
+	timerField: TimerModule
+	isTimerRunning: boolean = false
+
+	initInformation() {
+		this.stage = 1
+		this.timerField = new TimerModule()
+	}
+
+	startTimer(now: Date) {
+		this.timerField.init(now)
+		this.isTimerRunning = true
+	}
+
+	stopTimer() {
+		this.isTimerRunning = false
+	}
+
+	getFieldString(): string {
+		if (this.isTimerRunning) {
+			return `현재 소모된 시간: ${this.timerField.getDuration()}`
+		} else {
+			return `기록: ${this.timerField.getDuration()}`
+		}
+	}
+
+	getInformationRenderOutput(): string {
+		let output = '';
+
+		output += this.getFieldString()
+		output += '\n'
+		output += '\n'
+
+		return output
+	}
+}
+
